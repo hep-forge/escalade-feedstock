b85b1c95a6b3be13e95165ada74797158eac92a0
Marco Meyer-Conde <marco@tcu.ac.jp>
2025-08-26 22:07:00 +0900

    update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,16 +1,9 @@
 #! /usr/bin/bash
 
-echo 1
-# mkdir build
-# cd build
+mkdir build
+cd build
 
-# echo 2
-# cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX
+cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX
 
-# echo 3
-# make -j$(nproc)
-
-# echo 4
-# make install
-
-# echo 5
+make -j$(nproc)
+make install
